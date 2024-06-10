57c2e5df9a706960b68038622f5b8d03ce6bae26
Vanja <72052867+Nosliejnav@users.noreply.github.com>
2024-06-09 23:00:41 -0300

    Update SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -139,3 +139,29 @@ insert into pedido_exame values('','2022/12/22',0.00,22008,10050);
 
 select * from pedido_exame;
 
+-- 
+--
+
+-- Alteração de dados de tabelas -----------------
+select * from paciente;
+update paciente set nome_paciente = 'Aguinaldo Coelho' where cpf = '456.789.012-34';
+select * from paciente;
+
+select * from medico;
+update medico set especialidade = 'Ginecologia' where crm = 708090;
+select * from medico;
+
+select * from exame;
+update exame set preco = 135.00 where codigo = 10110;
+select * from exame;
+
+-- Exclusão de registros de tabelas --------------
+
+select * from paciente;
+delete from paciente where cpf = '456.789.012-34';
+select * from paciente;
+
+select * from medico;
+delete from medico where crm = 708090;
+select * from medico;
+
